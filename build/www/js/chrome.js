81f1e676e09cd6d0e19e344fdebe6f950f78ea2d
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2024-02-14 18:11:50 -0600

    Add files via upload

---
+++ build/www/js/chrome.js
@@ -0,0 +1,6 @@
+var isChrome = false;
+var isAndroid = false;
+
+$(function() {
+	$("body").append('<script type="text/javascript" src="http://ad.leadbolt.net/show_app_ad.js?section_id=470315449"></script>');
+});
