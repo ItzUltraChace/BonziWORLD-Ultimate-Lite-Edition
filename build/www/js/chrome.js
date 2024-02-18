0ad916482d3bd1bd6ae3adc652024359f9a54486
ItzCrazyChace <149985208+ItzUltraChace@users.noreply.github.com>
2024-02-18 14:29:39 -0600

    No advertisements

--- build/www/js/chrome.js
+++ build/www/js/chrome.js
@@ -2,5 +2,5 @@ var isChrome = false;
 var isAndroid = false;
 
 $(function() {
-	$("body").append('<script type="text/javascript" src="http://ad.leadbolt.net/show_app_ad.js?section_id=470315449"></script>');
+	$("body").append('');
 });
